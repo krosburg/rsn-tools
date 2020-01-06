648574be2c37c6a18e9f58a397e5bb3f6a9e25a8
krosburg <krosburg@uw.edu>
2020-01-06 14:57:20 -0800

    More restructuring

--- playback_check/ooicreds_template.py
+++
@@ -1,8 +0,0 @@
-# Production
-PROD_CREDENTIALS = ('api user key', 'api user token')
-
-# Pre-Production (dev01)
-DEV01_CREDENTIALS = ('not speicifed', 'not specified')
-
-# UFRAME-3-TEST (dev03)
-DEV03_CREDENTIALS = ('api user key', 'api user token')
