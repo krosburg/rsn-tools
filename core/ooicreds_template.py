3de6c58e20faea9bc5be7126db0e2473ba349277
krosburg <krosburg@uw.edu>
2020-02-10 15:11:38 -0800

    Updated credentials template

--- core/ooicreds_template.py
+++ core/ooicreds_template.py
@@ -2,7 +2,10 @@
 PROD_CREDENTIALS = ('api user key', 'api user token')
 
 # Pre-Production (dev01)
-DEV01_CREDENTIALS = ('not speicifed', 'not specified')
+DEV01_CREDENTIALS = ('api user key', 'api user token')
 
 # UFRAME-3-TEST (dev03)
 DEV03_CREDENTIALS = ('api user key', 'api user token')
+
+# Username
+OOI_USERNAME = 'username'
